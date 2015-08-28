8f26e08ebc6eafd7ecf09f7245c036876048e60f
root <root@r2s5.test.com>
2015-08-28 15:53:36 +0800

    revised with more reasonable name for schema.sql

--- project-code/bcia/t3c-etl/create_schema.sh
+++ project-code/bcia/t3c-etl/create_schema.sh
@@ -10,7 +10,7 @@ $HADOOP_HOME/bin/hadoop fs -mkdir -p $hdfs_path/BARCODE_RECORD
 $HADOOP_HOME/bin/hadoop fs -mkdir -p $hdfs_path/LOG_SEC_SCOSPRSC
 
 $HIVE_HOME/bin/hive -e "create database if not exists stagedb location '$hdfs_path';"
-hive -hiveconf hdfs_path=$hdfs_path -hiveconf db_name=$db_name -f ./schema.sql
+hive -hiveconf hdfs_path=$hdfs_path -hiveconf db_name=$db_name -f ./db-schema.sql
 
 ## verify
 hive -e "use $db_name; show tables;"
