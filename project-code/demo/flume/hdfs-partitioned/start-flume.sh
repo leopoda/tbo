b1a75d1505866483225b8e02af1cff0f042a40e2
Jack Wang <leopoda@gmail.com>
2015-09-07 10:45:17 +0800

    modified script

--- project-code/demo/flume/hdfs-partitioned/start-flume.sh
+++ project-code/demo/flume/hdfs-partitioned/start-flume.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+rm -rf ~/.flume/file-channel/checkpoint/*
+rm -rf ~/.flume/file-channel/data/*
+ 
 export FLUME_HOME=/usr/hdp/current/flume-server
 flume-ng agent --conf-file ./spool-to-hdfs.properties --name agent1 --conf $FLUME_HOME/conf -Dflume.root.logger=INFO,console
 
