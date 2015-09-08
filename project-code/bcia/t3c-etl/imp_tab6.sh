500f01baea9423830b0dc17074852b6bde7958e1
Jack Wang <leopoda@gmail.com>
2015-09-08 17:08:02 +0800

    modified imp_tab6.sh

--- project-code/bcia/t3c-etl/imp_tab6.sh
+++ project-code/bcia/t3c-etl/imp_tab6.sh
@@ -6,11 +6,12 @@
 tab_name=`${xmlcmd} sel -t -v /config/etl/tables/tab[@id=6]/name ${etlconf}`
 
 target_dir=${hdfs_path}/${tab_name}
-hive_tab=${db_name}.${tab_name}
+# hive_tab=${db_name}.${tab_name}
 
 # move to backup folder
-$HADOOP_HOME/bin/hdfs dfs -mv $hdfs_path/${tab_name}/* $hdfs_path/${tab_name}_BAK
+# $HADOOP_HOME/bin/hdfs dfs -mv $hdfs_path/${tab_name}/* $hdfs_path/${tab_name}_BAK
 
+$HADOOP_HOME/bin/hadoop fs -rm -r -skipTrash $hdfs_path/${tab_name}/*
 $SQOOP_HOME/bin/sqoop import --connect ${uri} \
                              --username ${imp_usr} \
                              --password ${imp_passwd} \
@@ -21,9 +22,9 @@ $SQOOP_HOME/bin/sqoop import --connect ${uri} \
                              --null-non-string '\\N' \
                              --num-mappers 1 
 
-if [ $? -eq 0 ]; then 
-  $HADOOP_HOME/bin/hdfs dfs -rm -r -skipTrash $hdfs_path/${tab_name}_BAK/*
-  ./bgfs-ren.sh ${tab_name}
-else
-  $HADOOP_HOME/bin/hdfs dfs -mv $hdfs_path/${tab_name}_BAK/* $hdfs_path/${tab_name}
-fi
+# if [ $? -eq 0 ]; then 
+#   $HADOOP_HOME/bin/hdfs dfs -rm -r -skipTrash $hdfs_path/${tab_name}_BAK/*
+#   ./bgfs-ren.sh ${tab_name}
+# else
+#   $HADOOP_HOME/bin/hdfs dfs -mv $hdfs_path/${tab_name}_BAK/* $hdfs_path/${tab_name}
+# fi
