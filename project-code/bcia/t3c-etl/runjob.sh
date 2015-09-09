a0c63e9297267220a5dc76639a7591c847a780f0
Jack Wang <leopoda@gmail.com>
2015-09-09 14:57:49 +0800

    better info for job already running

--- project-code/bcia/t3c-etl/runjob.sh
+++ project-code/bcia/t3c-etl/runjob.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
 
 JOB_DIR=/bcia/ingest
-JOB_LOG=`date +%Y-%m-%d`.log
+JOB_LOG=`date +%Y%m%d%H`.log
 
-ps_out=`ps -ef | grep $1 | grep -v 'grep' | grep -v $0`
+ps_out=`ps -ef | grep $1 | grep -v 'grep' | grep -v $0` 
 result=$(echo $ps_out | grep "$1")
 
 if [[ "$result" != "" ]];then
-    echo "Running, skipped" >> ${JOB_DIR}/log/${JOB_LOG} 2>&1 
+    echo `date '+%Y-%m-%d %H:%M:%S'` warn: ingestion job is already running, skipped the running this time. >> ${JOB_DIR}/log/${JOB_LOG} 2>&1
 else
-    echo launch job...
+    echo `date '+%Y-%m-%d %H:%M:%S'` info: launched job $1 >> ${JOB_DIR}/log/${JOB_LOG} 2>&1
     nohup $1 >> ${JOB_DIR}/log/${JOB_LOG} 2>&1 &
 fi
-
-
