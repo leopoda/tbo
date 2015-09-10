3e3558f8955913b270767a14ec480c872eef71da
Jack Wang <leopoda@gmail.com>
2015-09-10 16:12:57 +0800

    bug fix for log on top of hour situation

--- project-code/bcia/t3c-etl/data_ingest.sh
+++ project-code/bcia/t3c-etl/data_ingest.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-./imp_tab1.sh
-./imp_tab2.sh
+# ./imp_tab1.sh
+# ./imp_tab2.sh
 ./imp_tab3.sh
 ./imp_tab4.sh
 ./imp_tab5.sh
--- project-code/bcia/t3c-etl/runjob.sh
+++ project-code/bcia/t3c-etl/runjob.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 
 JOB_DIR=/bcia/ingest
-JOB_LOG=`date +%Y%m%d%H`.log
+# JOB_LOG=`date +%Y%m%d%H`.log
 
 ps_out=`ps -ef | grep $1 | grep -v 'grep' | grep -v $0`
 result=$(echo $ps_out | grep "$1")
 
 if [ "$result" != "" ]; then
-    echo `date '+%Y-%m-%d %H:%M:%S'` warn: ingestion job is already running, skipped the running this time. >> ${JOB_DIR}/log/${JOB_LOG} 2>&1
+    echo `date '+%Y-%m-%d %H:%M:%S'` warn: ingestion job is already running, skipped the running this time. >> ${JOB_DIR}/log/`date +%Y%m%d%H`.log 2>&1
 else
-    echo `date '+%Y-%m-%d %H:%M:%S'` info: launched job $1 >> ${JOB_DIR}/log/${JOB_LOG} 2>&1
-    nohup $1 >> ${JOB_DIR}/log/${JOB_LOG} 2>&1 &
+    echo `date '+%Y-%m-%d %H:%M:%S'` info: launched job $1 >> ${JOB_DIR}/log/`date +%Y%m%d%H`.log 2>&1
+    nohup $1 >> ${JOB_DIR}/log/`date +%Y%m%d%H`.log 2>&1 &
 fi
