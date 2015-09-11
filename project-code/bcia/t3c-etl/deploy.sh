667d3c509c706266f067719ac9035c9e2801bb4d
Jack Wang <leopoda@gmail.com>
2015-09-11 14:23:19 +0800

    changed field type definition to string for safe_time of table log_sec_ajxxb

--- project-code/bcia/t3c-etl/deploy.sh
+++ project-code/bcia/t3c-etl/deploy.sh
@@ -9,7 +9,7 @@ mkdir -p $deploy_dir/log
 cp ./*.sh $deploy_dir
 cp ./*.xml $deploy_dir
 
-./create_stagedb_tabs.sh
+./create_stagedb.sh
 
 rm -rf $deploy_dir/deploy.sh
 rm -rf $deploy_dir/trunc_tabs.sh
