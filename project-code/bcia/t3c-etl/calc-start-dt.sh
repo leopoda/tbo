57248f191e49dfcf058310763091efe9e06d85db
Jack Wang <leopoda@gmail.com>
2015-09-14 11:15:25 +0800

    fixed issue ParseException cannot recognize input near '<EOF>' '<EOF>' '<EOF>' for script calc-start-dt.sh

--- project-code/bcia/t3c-etl/calc-start-dt.sh
+++ project-code/bcia/t3c-etl/calc-start-dt.sh
@@ -3,7 +3,8 @@
 . ./config.sh
 
 tab_name=$1
-max_part=`${HIVE_HOME}/bin/hive -S -e "use ${db_name}; show partitions ${tab_name};" | sort -r | head -n 1`
+sql="use ${db_name}; show partitions ${tab_name};"
+max_part=`${HIVE_HOME}/bin/hive -S -e  "$sql" | sort -r | head -n 1`
 
 # max_part='lk_date=2014-08-15/lk_hour=08/lk_segmt=3'
 part_dt=`echo $max_part | awk -F'/' '{printf("%s=%s=%s", $1,$2,$3);}' | awk -F'=' '{printf("%s %s:%s:00", $2,$4,$6*10);}'`
