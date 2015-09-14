19faca7a18349c228b92bedc01a2cac8a7c9c257
Jack Wang <leopoda@gmail.com>
2015-09-14 17:27:53 +0800

    fixed no partition issue for table gat

--- project-code/bcia/t3c-etl/calc-start-dt.sh
+++ project-code/bcia/t3c-etl/calc-start-dt.sh
@@ -9,7 +9,7 @@ max_part=`${HIVE_HOME}/bin/hive -S -e  "$sql" | sort -r | head -n 1`
 # max_part='lk_date=2014-08-15/lk_hour=08/lk_segmt=3'
 part_dt=`echo $max_part | awk -F'/' '{printf("%s=%s=%s", $1,$2,$3);}' | awk -F'=' '{printf("%s %s:%s:00", $2,$4,$6*10);}'`
 
-if [ "$part_dt" != "" ]; then
+if [ "$max_part" != "" ]; then
   echo $part_dt
 else
   echo '1900-01-01 00:00:00'
