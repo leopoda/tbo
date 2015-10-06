437ec81e88fda59c635c8f06de6a39091a81c201
Jack Wang <leopoda@gmail.com>
2015-10-06 17:09:32 +0800

    revised text format for class Validator

--- project-code/demo/flume/sql-source/src/main/java/cn/rtmap/flume/validation/Validator.java
+++ project-code/demo/flume/sql-source/src/main/java/cn/rtmap/flume/validation/Validator.java
@@ -15,32 +15,31 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public abstract class Validator implements Interceptor {
     private static final Logger LOG = LoggerFactory.getLogger(Validator.class);
 
-	// only Builder can build me.
-	protected Validator() {}
-	
-	@Override
-	public void initialize() {}
+    // only Builder can build me
+    // protected Validator() {}
 
-  @Override
-  public Event intercept(Event event) {
+    @Override
+    public void initialize() {}
 
-    Map<String, String> headers = event.getHeaders();
-    String body = new String(event.getBody());
-    if (!validate(body)) {
-		LOG.error("data validation failed: {}", body);
-		headers.put("validation", "1");
-	} else {
-		headers.put("validation", "0");
-	}
-    return event;
-  }
+    @Override
+    public Event intercept(Event event) {
+        Map<String, String> headers = event.getHeaders();
+        String body = new String(event.getBody());
+        if (!validate(body)) {
+            LOG.error("data validation failed: {}", body);
+            headers.put("validation", "1");
+        } else {
+            headers.put("validation", "0");
+        }
+        return event;
+    }
 
     @Override
     public List<Event> intercept(List<Event> events) {
-      for (Event event : events) {
-        intercept(event);
-      }
-      return events;
+        for (Event event : events) {
+            intercept(event);
+        }
+        return events;
     }
 
     @Override
@@ -48,18 +47,17 @@ public abstract class Validator implements Interceptor {
 
     public abstract boolean validate(Object data);
 
-	 public static class Builder implements Interceptor.Builder {
+    public static class Builder implements Interceptor.Builder {
+        @Override
+        public Validator build() {
+            ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("context.xml");
+            Validator bean = (Validator)context.getBean("validator");
+            context.close();
 
-	 	@Override
-	 	public Validator build() {
-	 		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("context.xml");
-	 		Validator bean = (Validator)context.getBean("validator");
-	 		context.close();
+            return bean;
+        }
 
-	 		return bean;
-	 	}
-
-	 	@Override
-	 	public void configure(Context context) {}
-	 }
+        @Override
+        public void configure(Context context) {}
+    }
 }
