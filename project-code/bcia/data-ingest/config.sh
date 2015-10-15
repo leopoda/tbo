6cf6b06bc6a7744610c52d8ea92c0cf29b392ed4
Jack Wang <leopoda@gmail.com>
2015-10-15 18:27:26 +0800

    optimized config for monitoring and stress testing

---
+++ project-code/bcia/data-ingest/config.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+export FLUME_HOME=/usr/iop/current/flume-server
+export status_file_path=/var/lib/flume
+export ojdbc_path=../../../../tools/oracle/orc-jdbc
+export sql_source_jar=target/flume-ng-sql-source-1.3-SNAPSHOT.jar
+export plugin_dir=${FLUME_HOME}/plugins.d
+
+export JAVA_OPTS="-Xms100m -Xmx4096m"
--- project-code/bcia/data-ingest/deploy-sqlsource.sh
+++ project-code/bcia/data-ingest/deploy-sqlsource.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 
-export FLUME_HOME=/usr/iop/current/flume-server
-export status_file_path=/var/lib/flume
-export ojdbc_path=../../../../tools/oracle/orc-jdbc
-export sql_source_jar=target/flume-ng-sql-source-1.3-SNAPSHOT.jar
-export plugin_dir=${FLUME_HOME}/plugins.d
+. ./config.sh
 
 rm -rf ${status_file_path}/*
 rm -rf ${plugin_dir}/sql-source/lib/*
