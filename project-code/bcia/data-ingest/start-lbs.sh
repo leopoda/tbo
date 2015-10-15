9b477f7a1a45b0f6474916d8603a79227dab98a7
Jack Wang <leopoda@gmail.com>
2015-10-15 18:32:27 +0800

    optimized script for stress testing

--- project-code/bcia/data-ingest/start-lbs.sh
+++ project-code/bcia/data-ingest/start-lbs.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
 export FLUME_HOME=/usr/iop/current/flume-server
+#export JAVA_OPTS="-Xms100m -Xmx4096m"
 
-flume-ng agent --conf-file ./lkxxb.conf --name agent4 --conf $FLUME_HOME/conf -Dflume.root.logger=INFO,console
+flume-ng agent --conf-file ./lkxxb.conf --name agent4 --conf $FLUME_HOME/conf -Dflume.monitoring.type=http -Dflume.monitoring.port=41412 -Dflume.root.logger=INFO,console
